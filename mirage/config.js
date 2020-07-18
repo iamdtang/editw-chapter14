6e0d8f80568ed919112c9aaa43f5bf7d085c5f0b
David Tang <dtang@auditboard.com>
2020-07-18 00:01:05 -0700

    Adds demo app with polymorphic relationships

---
+++ mirage/config.js
@@ -0,0 +1,84 @@
+export default function () {
+  this.get('/comments', function () {
+    return {
+      comments: [
+        {
+          id: '1',
+          body: 'Looking forward to using all the new features!',
+          content: { id: '1', type: 'post' }
+          // content: 'post:1'
+        },
+        {
+          id: '2',
+          body: 'Great video! Please make more!',
+          content: { id: '2', type: 'video' }
+          // content: 'video:2'
+        }
+      ],
+      posts: [{ id: '1', title: 'Ember Octane Released', comments: ['1'] }],
+      videos: [{ id: '2', title: 'Introduction to Ember.js', comments: ['2'] }]
+    };
+  });
+
+  this.post('/comments', function (schema, request) {
+    return {
+      comment: Object.assign(
+        {
+          id: Math.random()
+        },
+        JSON.parse(request.requestBody)
+      )
+    };
+  });
+
+  this.get('/users/:id', function () {
+    return {
+      users: {
+        id: '1',
+        name: 'David',
+        purchasedProducts: [
+          { id: '5', type: 'course' },
+          { id: '10', type: 'book' }
+        ]
+      }
+    };
+  });
+
+  this.get('/products', function () {
+    return {
+      courses: [
+        {
+          id: '5',
+          title: 'Introduction to Ember Octane',
+          length: '2 hours'
+        },
+        {
+          id: '15',
+          title: 'Getting started with Glimmer Components',
+          length: '1 hour'
+        }
+      ],
+      books: [
+        {
+          id: '10',
+          title: 'Ember Data in the Wild',
+          pages: 100
+        }
+      ]
+      // products: [
+      //   {
+      //     id: '5',
+      //     type: 'course',
+      //     title: 'Introduction to Ember Octane',
+      //     length: '2 hours'
+      //   },
+      //   {
+      //     id: '10',
+      //     type: 'book',
+      //     title: 'Ember Data in the Wild',
+      //     pages: 100
+      //   }
+      // ]
+    };
+  });
+}
